69784b02d06733cc12765f74642d6301e25045b1
heejung-gjt <hee_9803@naver.com>
2021-11-24 21:43:47 +0900

    feat: add user and cars model
    - User 모델 작성
    - Car 모델 작성

--- users/models.py
+++ users/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+
+class User(models.Model):
+    id       = models.CharField(primary_key=True, unique=True, max_length=32)
+    password = models.CharField(max_length=64)
+    car      = models.ForeignKey("cars.Car", null=True, on_delete=models.SET_NULL)
+
